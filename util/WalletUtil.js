6b37606abc13fc16377352a77a68a607148c9ecd
Robert Schuh <gitlab@eneticum.de>
2016-10-13 16:09:35 +0200

    write all transactions to the db

--- util/WalletUtil.js
+++ util/WalletUtil.js
@@ -12,7 +12,7 @@
 		static loadUpdates () {
 			request(`${config.ccp.api.url}/Corp/WalletJournal.xml.aspx?keyID=${config.ccp.keyID}&vCode=${config.ccp.vCode}`, (error, response, body) => {
 				if (!error && response.statusCode == 200) {
-					parseString(body, (parseError, result) => {
+					parseString(body, async (parseError, result) => {
 						if(parseError || !result.eveapi) {
 							console.log(parseError || result);
 							return setTimeout(() => WalletUtil.startUpdater(), 5 * 1000);
@@ -20,8 +20,8 @@
 							let d = new Date(result.eveapi.cachedUntil[0] + "Z");
 							let e = new Date(result.eveapi.currentTime[0] + "Z");
 							if(!result.eveapi.error) {
-								console.log(JSON.stringify(result.eveapi.result[0].rowset[0].row || []));
-								(result.eveapi.result[0].rowset[0].row || [])
+
+								let transactions = (result.eveapi.result[0].rowset[0].row || [])
 									.filter(({ $ }) => $.ownerName2 == "ISKstarter")
 									.map(({ $ }) => ({
 										fromID: 	$.ownerID2 - 0,
@@ -29,15 +29,31 @@
 										toID: 		$.ownerID1 - 0,
 										toName: 	$.ownerName1,
 										refID: 		$.refID - 0,
-										amount: 	$.amount - 0,
+										amount: 	(($.amount - 0) * (100 - config.site.tax) | 0) / 100,
 										reason: 	$.reason,
 										timestamp: 	new Date($.date + "Z").getTime()
-									}))
-									.map(e => DBUtil
-										.getCollection("transactions")
-										.then(collection => collection.update({ refID: e.refID }, { $setOnInsert: e }, { upsert: true }))
-										.catch(e => console.log(e))
-									);
+									}));
+
+								const transactionCollection = await DBUtil.getCollection("transactions");
+								const entityCollection = await DBUtil.getCollection("entities");
+
+								await Promise.all(transactions.map(transaction => transactionCollection.update({ refID: transaction.refID }, { $setOnInsert: transaction }, { upsert: true })));
+
+								await Promise.all(transactions.filter(transaction => transaction.reason != "").map(async (transaction) => {
+									let entity = await entityCollection.findOne({ _id: DBUtil.to_id(transaction.reason) });
+									if(entity) {
+										await transactionCollection.update({ toRefID: transaction.refID }, { $setOnInsert: {
+											fromID: transaction.toID,
+											fromName: transaction.toName,
+											toID: entity._id,
+											toName: entity.name,
+											toRefID: transaction.refID,
+											amount: transaction.amount,
+											reason: "[donation]"
+										} }, { upsert: true });
+									}
+								}));
+
 							} else {
 								console.log(result.eveapi.error);
 							}
