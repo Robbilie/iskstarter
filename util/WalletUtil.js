21bcb76fee23d1a3f05d25078f6a05a44eeed0ab
Robert Schuh <gitlab@eneticum.de>
2016-11-18 17:25:59 +0100

    some nav tweaks

--- util/WalletUtil.js
+++ util/WalletUtil.js
@@ -34,6 +34,95 @@
 			});
 		}
 
+		static get_data () {
+			return new Promise((resolve, reject) => {
+				request(`${process.env.XML_URL}/Corp/WalletJournal.xml.aspx?keyID=${config.xml.keyID}&vCode=${config.xml.vCode}`, (error, response, body) => {
+					if (!error && response.statusCode == 200) {
+						parseString(body, async (parseError, result) => {
+							if(parseError || !result.eveapi) {
+								console.log(parseError || result);
+								reject(parseError);
+							} else {
+
+								let d = new Date(result.eveapi.cachedUntil[0] + "Z");
+
+								let obj = (result.eveapi.result[0].rowset[0].row || [])
+									.map(({ $ }) => ({
+										fromID: 	$.ownerID1 - 0,
+										fromName: 	$.ownerName1,
+										toID: 		$.ownerID2 - 0,
+										toName: 	$.ownerName2,
+										refID: 		$.refID - 0,
+										amount: 	$.amount - 0,
+										reason: 	$.reason.replace("DESC:", "").trim(),
+										timestamp: 	new Date($.date + "Z").getTime()
+									}));
+
+								resolve({
+									headers: {
+										expires: d.toString()
+									},
+									obj
+								});
+							}
+						});
+					} else {
+						console.log(error || response.statusCode || response);
+						reject(error);
+					}
+				});
+			});
+		}
+
+		static async load_updates () {
+			try {
+
+				let response = await WalletUtil.get_data();
+
+				const transactionCollection = await DBUtil.getCollection("transactions");
+				const entityCollection = await DBUtil.getCollection("entities");
+
+				await Promise.all(response.obj.map(async transaction => {
+
+					// create pay ins
+					if(transaction.toName == "ISKstarter") {
+						await transactionCollection.update(
+							{ refID: transaction.refID },
+							{
+								$setOnInsert: {
+									fromName: "EVE System",
+									fromID: 1,
+									toName: transaction.fromName,
+									toID: transaction.toID,
+									amount: Math.floor((transaction.amount - 0) * (100 - parseFloat(process.env.TAX))) / 100,
+								}
+							},
+							{ upsert: true }
+						);
+					}
+
+					let entity = await entityCollection.findOne({ _id: DBUtil.to_id(transaction.reason) });
+
+					// convert pay ins to donations
+					if(transaction.toName == "ISKstarter" && entity && entity.data.start < transaction.timestamp && entity.data.end > transaction.timestamp) {
+
+					}
+
+					// create pay outs
+					if(transaction.fromName == "ISKstarter" && entity) {
+
+					}
+
+				}));
+
+				setTimeout(() => WalletUtil.load_updates(), new Date(response.headers.expires).getTime() - Date.now());
+
+			} catch (e) {
+				console.log(e);
+				setTimeout(() => WalletUtil.load_updates(), 5 * 1000);
+			}
+		}
+
 		static loadUpdates () {
 			request(`${process.env.XML_URL}/Corp/WalletJournal.xml.aspx?keyID=${config.xml.keyID}&vCode=${config.xml.vCode}`, (error, response, body) => {
 				if (!error && response.statusCode == 200) {
